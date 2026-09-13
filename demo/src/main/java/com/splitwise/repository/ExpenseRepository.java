c356e2914c74f0662bd41d8f2225016607a37f34
Neelesh Sonwani <sonwanineelesh@gmail.com>
2026-09-14 02:12:52 +0530

    added dashboard apis

--- demo/src/main/java/com/splitwise/repository/ExpenseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.splitwise.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.splitwise.entity.Expense;
-
-public interface ExpenseRepository extends CrudRepository<Expense, Long>{
-
-}
--- demo/src/main/java/com/splitwise/service/ExpenseService.java
+++
@@ -1,9 +0,0 @@
-package com.splitwise.service;
-
-import com.splitwise.DTO.ExpenseDTO;
-
-public interface ExpenseService {
-
-	String addExpense(ExpenseDTO expenseDto);
-
-}
